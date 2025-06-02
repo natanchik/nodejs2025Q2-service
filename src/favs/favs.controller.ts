311d270b5137b161c21f03d0020c01750542052e
Natalia Lebedeva <nataliadaveng@gmail.com>
2025-06-02 23:00:52 +0500

    fix: remove unused import

--- src/favs/favs.controller.ts
+++ src/favs/favs.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Param, Delete } from '@nestjs/common';
 import { FavsService } from './favs.service';
 
 @Controller('favs')
