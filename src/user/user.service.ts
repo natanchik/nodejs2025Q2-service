6aeffc07ebeec4cdcf164db02aa8e24bd96d686f
Natalia Lebedeva <nataliadaveng@gmail.com>
2025-06-08 13:58:20 +0500

    feat: update user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,29 +6,36 @@ import {
 } from '@nestjs/common';
 import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
 
-import { users } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  getUsers() {
-    return Object.values(users);
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  getUsers(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
-  getUserById(id: string) {
+  async getUserById(id: string): Promise<User> {
     if (uuidValidate(id)) {
-      if (id in users) {
-        return users[id];
-      } else {
+      const user = await this.userRepository.findOne({ where: { id } });
+      if (!user) {
         throw new NotFoundException('User is not found');
       }
+      return user;
     } else {
       throw new BadRequestException('User id is not correct');
     }
   }
 
-  createUser(createUserDto: CreateUserDto) {
+  createUser(createUserDto: CreateUserDto): Promise<User> {
     if (
       'login' in createUserDto &&
       'password' in createUserDto &&
@@ -36,50 +43,45 @@ export class UserService {
       createUserDto.password
     ) {
       const id = uuidv4();
-      users[id] = {
+      const newUser = this.userRepository.create({
         id: id,
         login: createUserDto.login,
         password: createUserDto.password,
         version: 1,
         createdAt: Date.now(),
         updatedAt: Date.now(),
-      };
-      return {
-        id: id,
-        login: users[id].login,
-        version: 1,
-        createdAt: users[id].createdAt,
-        updatedAt: users[id].updatedAt,
-      };
+      });
+      return this.userRepository.save(newUser);
     } else {
       throw new BadRequestException('Request is not correct');
     }
   }
 
-  updatePassword(updatePasswordDto: UpdatePasswordDto, id: string) {
+  async updatePassword(
+    updatePasswordDto: UpdatePasswordDto,
+    id: string,
+  ): Promise<User> {
     if (
       'oldPassword' in updatePasswordDto &&
       'newPassword' in updatePasswordDto
     ) {
       if (uuidValidate(id)) {
-        if (id in users) {
-          if (users[id].password === updatePasswordDto.oldPassword) {
-            users[id].password = updatePasswordDto.newPassword;
-            users[id].version = ++users[id].version;
-            users[id].updatedAt = Date.now();
-
-            return {
-              id: id,
-              login: users[id].login,
-              version: users[id].version,
-              createdAt: users[id].createdAt,
-              updatedAt: users[id].updatedAt,
-            };
-          } else {
-            throw new ForbiddenException('Old password is not correct');
-          }
-        } else {
+        const user = await this.userRepository.findOne({ where: { id } });
+        if (!user) {
           throw new NotFoundException('User is not found');
+        }
+        if (user.password === updatePasswordDto.oldPassword) {
+          await this.userRepository.update(id, {
+            id,
+            login: user.login,
+            password: updatePasswordDto.newPassword,
+            version: ++user.version,
+            createdAt: user.createdAt,
+            updatedAt: Date.now(),
+          });
+          return this.userRepository.findOne({ where: { id } });
+        } else {
+          throw new ForbiddenException('Old password is not correct');
         }
       } else {
         throw new BadRequestException('User id is not correct');
@@ -89,10 +91,11 @@ export class UserService {
     }
   }
 
-  deleteUserById(id: string) {
+  async deleteUserById(id: string): Promise<void> {
     if (uuidValidate(id)) {
-      if (id in users) {
-        delete users[id];
+      const user = await this.userRepository.findOne({ where: { id } });
+      if (user) {
+        await this.userRepository.delete(id);
       } else {
         throw new NotFoundException('User is not found');
       }
