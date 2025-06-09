c30a04da06fad990f15ea7b57fe6a97894a0270b
Natalia Lebedeva <nataliadaveng@gmail.com>
2025-06-09 19:09:44 +0500

    fix: update modules

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TrackService } from './track.service';
 import { TrackController } from './track.controller';
 import { Track } from './entities/track.entity';
+import { Favorites } from 'src/favs/entities/fav.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Track])],
+  imports: [TypeOrmModule.forFeature([Track, Favorites])],
   controllers: [TrackController],
   providers: [TrackService],
   exports: [TrackService],
