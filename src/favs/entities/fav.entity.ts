705edcf284388cfcdad1712d8783306de8e5b7b1
Natalia Lebedeva <nataliadaveng@gmail.com>
2025-06-09 20:03:09 +0500

    fix: add PrimaryGeneratedColumn to Favorites entity

--- src/favs/entities/fav.entity.ts
+++ src/favs/entities/fav.entity.ts
@@ -1,10 +1,13 @@
-import { Entity, ManyToMany, JoinTable } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
 import { Artist } from '../../artist/entities/artist.entity';
 import { Album } from '../../album/entities/album.entity';
 import { Track } from '../../track/entities/track.entity';
 
 @Entity()
 export class Favorites {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
   @ManyToMany(() => Artist)
   @JoinTable()
   artists: Artist[];
