49cb31efce40b76d09ef921e994fa386b6ea6a84
Natalia Lebedeva <nataliadaveng@gmail.com>
2025-06-09 18:58:26 +0500

    feat: update modules

--- src/favs/favs.module.ts
+++ src/favs/favs.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavsService } from './favs.service';
 import { FavsController } from './favs.controller';
+import { Favorites } from './entities/fav.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Favorites])],
   controllers: [FavsController],
   providers: [FavsService],
+  exports: [FavsService],
 })
 export class FavsModule {}
